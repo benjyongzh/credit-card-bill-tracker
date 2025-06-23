ea71251745ece0c72aa4d890fee83446851462fb
benjyongzh <benj_yongzh@hotmail.com>
2025-06-23 23:22:37 +0800

    Add details for exceptions

--- server/src/main/java/com/credit_card_bill_tracker/backend/common/errors/UnauthorizedException.java
+++ server/src/main/java/com/credit_card_bill_tracker/backend/common/errors/UnauthorizedException.java
@@ -1,6 +1,12 @@
 package com.credit_card_bill_tracker.backend.common.errors;
 
+import java.util.List;
+
 public class UnauthorizedException extends AppException {
+    public UnauthorizedException(String message, List<String> details) {
+        super(message, 401, details);
+    }
+
     public UnauthorizedException(String message) {
         super(message, 401);
     }
