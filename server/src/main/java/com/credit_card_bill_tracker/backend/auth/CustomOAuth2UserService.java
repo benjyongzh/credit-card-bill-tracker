2124ba53f8c5f0330e84147d88d3c040ebf229b8
benjyongzh <benj_yongzh@hotmail.com>
2025-07-08 22:57:44 +0800

    Fix CustomOAuth2UserService

--- server/src/main/java/com/credit_card_bill_tracker/backend/auth/CustomOAuth2UserService.java
+++ server/src/main/java/com/credit_card_bill_tracker/backend/auth/CustomOAuth2UserService.java
@@ -4,25 +4,25 @@ import com.credit_card_bill_tracker.backend.user.UserDTO;
 import com.credit_card_bill_tracker.backend.user.UserRepository;
 import com.credit_card_bill_tracker.backend.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.oidc.OidcUser;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OidcUser> {
+public class CustomOAuth2UserService implements OAuth2UserService<OidcUserRequest, OidcUser> {
 
     private final UserRepository userRepository;
     private final UserService userService;
     private final OidcUserService delegate = new OidcUserService();
 
     @Override
-    public OidcUser loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
         OidcUser oidcUser = delegate.loadUser(userRequest);
         String email = oidcUser.getEmail();
         userRepository.findByEmail(email).orElseGet(() -> {
