c7c2e5393bc3dce4a736745005f2af9fcf0e3bb8
benjyongzh <benj_yongzh@hotmail.com>
2025-07-16 22:19:08 +0800

    Fix unit test

--- server/src/test/java/com/credit_card_bill_tracker/backend/expensesummary/ExpenseSummaryServiceTests.java
+++ server/src/test/java/com/credit_card_bill_tracker/backend/expensesummary/ExpenseSummaryServiceTests.java
@@ -1,6 +1,6 @@
 package com.credit_card_bill_tracker.backend.expensesummary;
 
-import com.credit_card_bill_tracker.backend.common.errors.UnauthorizedException;
+import com.credit_card_bill_tracker.backend.common.errors.ForbiddenException;
 import com.credit_card_bill_tracker.backend.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class ExpenseSummaryServiceTests {
@@ -30,7 +31,7 @@ public class ExpenseSummaryServiceTests {
         user.setId(UUID.randomUUID());
         UUID otherId = UUID.randomUUID();
 
-        assertThrows(UnauthorizedException.class, () -> service.getAllSummaries(user, otherId));
+        assertThrows(ForbiddenException.class, () -> service.getAllSummaries(user, otherId));
         verify(repository, never()).findByUserId(any());
     }
 
